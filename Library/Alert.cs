ca78b5d94e4b7aac1a096d011c4a227825590ec3
Mateone <54219667+Mateone@users.noreply.github.com>
2020-09-18 20:49:55 -0300

    Comments about Polymorfism and LSP

--- Library/Alert.cs
+++ Library/Alert.cs
@@ -3,7 +3,12 @@ using System;
 namespace LSPLibrary
 {
     public class Alert
-    {
+    { 
+        /*
+            La clase implementa un metodo "Send", el cual cambia su acción dependiendo
+            del "type" que se le pase por parametro. Esto debería estar implementado en
+            dos clases diferentes utilizando la misma operación polimórfica.
+        */
         public void Send(string type, string text)
         {
             if (type == "trello")
--- Library/Event.cs
+++ Library/Event.cs
@@ -2,6 +2,12 @@ namespace LSPLibrary
 {
     public class Event
     {
+        /*
+            La clase implementa un metodo "Notify", que cambia su funcionamiento
+            dependiendo del "EventType", por lo cual esto se podría separar en dos clases
+            diferentes que implementen una interfaz IEvent, con metodos distintas
+            para la operación "Notify".
+        */
         public string EventName { get; set; }
         public string EventType { get; set; }
 
